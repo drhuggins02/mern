b5511bda4ed5c5da8e5f7ac2f701a24a14f7426d
root <root@LAPTOP-UOGRUV9S.localdomain>
2019-12-07 22:09:59 -0500

    This is the completed database setup

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const morgan = require("morgan");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// DATABASE CONNECTIONS
+mongoose.connect(
+    process.env.MONGO_URI, 
+    { useUnifiedTopology: true, useNewUrlParser: true }
+)
+.then(() => console.log('Database Connected'))
+
+mongoose.connection.on('error', err => {
+    console.log(`Database connection error: ${err.message}`)
+});
 
 // BRING IN ROUTES
-const { getPosts } = require('./routes/post');
+const postRoutes = require('./routes/post');
 
 // MIDDLEWARE
 app.use(morgan('dev'));
+app.use("/", postRoutes);
 
-app.get("/", getPosts);
-
-const port = 8080;
-
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Node JS API is listening on port: ${port}`);
 });
